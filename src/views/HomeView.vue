99d6ad066cccd362fe0bea928f48621ff6c5f7cf
Tom Chanson <tom.chanson@epsi.fr>
2023-12-21 09:50:39 +0100

    ajout de unocss

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,6 +3,10 @@
 
 <template>
   <div class="wrapper">
-    <h1>Home</h1>
+    <h1>Home classe avec tailwind (uno.config)</h1>
+    <div class="flex flex-col">
+      <RouterLink to="/about">About</RouterLink>
+      <RouterLink to="/contact">Contact</RouterLink>
+    </div>
   </div>
 </template>
