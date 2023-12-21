5f9cd67909e2a4458e5896173c0d214021a23183
Tom Chanson <tom.chanson@epsi.fr>
2023-12-21 09:40:27 +0100

    clean

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import DestinationView from '../views/DestinationView.vue'
-import BarChart from '../views/BarChartView.vue'
 
 
 const router = createRouter({
@@ -11,21 +9,6 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      component: () => import('../views/AboutView.vue')
-    },
-    {
-      path: '/destination/:pays',
-      name: 'destination',
-      component: DestinationView
-    },
-    {
-      path: '/bar-chart',
-      name: 'chart',
-      component: BarChart
     }
   ]
 })
