64003fd95777818662186f0b21f5c12ba236a5fe
PIERRE Simon <s.pierre@altior.fr>
2023-12-21 11:48:13 +0100

    allPokemon

---
+++ src/components/AllPokemon.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h3>{{ pokemon.name }}</h3>
+    <p>ID: {{ pokemon.id }}</p>
+    <img :src="pokemon.imageUrl" :alt="pokemon.name" />
+  </div>
+</template>
+
+<script>
+import {fetchDetailPokemon} from "../function";
+export default {
+  data() {
+    return {
+      pokemon: fetchDetailPokemon(this.pokemon)
+    }
+  },
+  props: {
+    urlPokemon: {
+      type: Object,
+      required: true
+    }
+  },
+  computed: {
+    imageUrl() {
+      return `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${this.pokemon.id}.png`;
+    }
+  }
+}
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,24 +1,31 @@
+<template>
+  <div>
+    <PokemonDetails v-for="pokemon in listDetailPokemon" :key="pokemon.url" :urlPokemon="pokemon.url" />
+  </div>
+</template>
+
 <script>
+import axios from 'axios';
+import PokemonDetails from '../components/AllPokemon.vue';
+
 import {fetchListPokemon} from "../function";
 export default {
+  components: {
+    PokemonDetails
+  },
   data() {
     return {
-      detailPokemon: {},
-      listDetailPokemon: [],
-    }
+      listDetailPokemon: []
+    };
   },
   methods: {
     fetchPokemon() {
       this.listDetailPokemon = fetchListPokemon();
     }
   },
-  
+
   created: function () {
     this.fetchPokemon();
   }
 }
 </script>
-
-<template>
-  <div></div>
-</template>
