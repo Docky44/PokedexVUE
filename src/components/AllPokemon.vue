5dc9d2b29acf80790d21b70b467dc4e5d0a14c4e
Tom Chanson <tom.chanson@epsi.fr>
2023-12-22 08:31:42 +0100

    Add style import and search functionality

--- src/components/AllPokemon.vue
+++ src/components/AllPokemon.vue
@@ -1,18 +1,28 @@
-<template>
-  <div>
-    <h3>{{ pokemon.name }}</h3>
-    <p>ID: {{ pokemon.id }}</p>
-    <img :src="imageUrl" :alt="pokemon.name" />
-  </div>
-</template>
+<script setup>
+import { RouterLink } from 'vue-router'
+</script>
+<style>
+@import '../assets/css/style.css';
+
+</style>
 
 <script>
+
 import {fetchDetailPokemon} from "../function";
 export default {
   data() {
     return {
-      pokemon: {}
+      pokemon: {
+        id: 0,
+        name: '',
+        imageUrl: '',
+        types: [{
+          type: {
+            name: ''
+          }
+        }]
     }
+  };
   },
   props: {
     urlPokemon: {
@@ -36,3 +46,20 @@ export default {
   }
 }
 </script>
+
+<template>
+  <li>
+  <RouterLink :to="{ name: 'pokemon-details', params: { id: pokemon.id } }" class="pokemon-card">
+    <img :src="imageUrl" :alt="pokemon.name" class="pokemon-image-card">
+    <div class="pokemon-info">
+        <p class="id">
+            <span class="number-prefix">N°{{ pokemon.id }}</span>
+        </p>
+        <h5>{{ pokemon.name }}</h5>
+        <div class="types">
+          <span v-for="type in pokemon.types" :key="type" class="type" :class="type.type.name">{{ type.type.name }}</span>
+        </div>
+    </div>
+  </RouterLink>
+</li>
+</template>
